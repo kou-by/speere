bc6de35e15b9247deae458236fe528a24b0e7076
Takashi Fujita <tgfjt.mail@gmail.com>
2025-03-31 20:18:27 +0900

    fix: align tsup output extension with package.json exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts', 'src/grammar.ts'],
   format: ['cjs', 'esm'],
+  // Add outExtension to specify .mjs for esm format
+  outExtension({ format }) {
+    return {
+      js: `.${format === 'esm' ? 'mjs' : 'cjs'}`,
+    }
+  },
   dts: true,
   splitting: false,
   sourcemap: true,
   clean: true,
-});
-
+})
